8348bcdd2d1301c999162ddcfa1f59b35bbc0e03
rahul2004a <kumar.rahul.2017354@gmail.com>
2025-05-31 20:08:05 +0530

    Fetching all URLs of the User

--- Backend/src/main/java/com/url/shortener/controller/UrlMappingController.java
+++ Backend/src/main/java/com/url/shortener/controller/UrlMappingController.java
@@ -8,12 +8,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -34,5 +32,13 @@ public class UrlMappingController {
         User user = userService.findByUsername(principal.getName());
         UrlMappingDTO urlMappingDTO = urlMappingService.createShortUrl(originalUrl,user);
         return ResponseEntity.ok(urlMappingDTO);
+    }
+
+    @GetMapping("/myurls")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<UrlMappingDTO>> getUserUrls(Principal principal){
+        User user = userService.findByUsername(principal.getName());
+        List<UrlMappingDTO> url = urlMappingService.getUrlByUser(user);
+        return ResponseEntity.ok(url);
     }
 }
--- Backend/src/main/java/com/url/shortener/service/UrlMappingService.java
+++ Backend/src/main/java/com/url/shortener/service/UrlMappingService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Random;
 
 @Service
@@ -49,5 +50,11 @@ public class UrlMappingService {
             shortUrl.append(characters.charAt(random.nextInt(characters.length())));
         }
         return shortUrl.toString();
+    }
+
+    public List<UrlMappingDTO> getUrlByUser(User user) {
+        return urlMappingRepository.findByUser(user).stream()
+                .map(this::convertToDto)
+                .toList();
     }
 }
