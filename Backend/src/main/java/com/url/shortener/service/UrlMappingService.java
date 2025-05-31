4e93fad02fcea9329634010a2ed96299757a2d4c
rahul2004a <kumar.rahul.2017354@gmail.com>
2025-05-31 23:25:14 +0530

    URL Redirect Functionality

--- Backend/src/main/java/com/url/shortener/service/UrlMappingService.java
+++ Backend/src/main/java/com/url/shortener/service/UrlMappingService.java
@@ -90,4 +90,18 @@ public class UrlMappingService {
         return clickEvents.stream()
                 .collect(Collectors.groupingBy(click -> click.getClickDate().toLocalDate(), Collectors.counting()));
     }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if(urlMapping!=null){
+            urlMapping.setClickCount(urlMapping.getClickCount()+1);
+            urlMappingRepository.save(urlMapping);
+
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
+        return urlMapping;
+    }
 }
