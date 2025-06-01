f432c4a3aa012bfdc20bea990f69a6425ac43441
rahul2004a <kumar.rahul.2017354@gmail.com>
2025-06-01 23:18:09 +0530

    SettingUp Landing Page and About page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './pages/LandingPage'
+import AboutPage from './pages/AboutPage'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   )
 }
