100928ba23b3f0f4416422d28ef49d5e79bfda11
rahul2004a <kumar.rahul.2017354@gmail.com>
2025-06-02 00:27:03 +0530

    Register Page and register user through frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,8 @@ import LandingPage from './pages/LandingPage'
 import AboutPage from './pages/AboutPage'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Register from './pages/Register'
+import Loginpage from './pages/Loginpage'
 
 function App() {
 
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path='/about' element={<AboutPage />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Loginpage />} />
         </Routes>
         <Footer />
       </Router>
