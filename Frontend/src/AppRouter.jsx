41e492bdc804044453830e495a8e22ca93f2a245
rahul2004a <kumar.rahul.2017354@gmail.com>
2025-06-05 01:44:37 +0530

    Managing Private and Public route

--- Frontend/src/AppRouter.jsx
+++ Frontend/src/AppRouter.jsx
@@ -8,6 +8,7 @@ import DashboardLayout from './pages/Dashboard/DashboardLayout';
 import Footer from './components/Footer';
 import { Toaster } from 'react-hot-toast';
 import ShortenUrlPage from './pages/ShortenUrlPage';
+import PrivateRoute from './PrivateRoute';
 
 
 const AppRouter = () => {
@@ -18,9 +19,22 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path='/about' element={<AboutPage />} />
-                <Route path='/register' element={<RegisterPage />} />
-                <Route path='/login' element={<LoginPage />} />
-                <Route path='/dashboard/*' element={<DashboardLayout />} />
+                <Route path="/register" element={
+                    <PrivateRoute publicPage={true}>
+                        <RegisterPage />
+                    </PrivateRoute>
+                } />
+                <Route path="/login" element={
+                    <PrivateRoute publicPage={true}>
+                        <LoginPage />
+                    </PrivateRoute>
+                } />
+                <Route path="/dashboard" element={
+                    <PrivateRoute publicPage={false}>
+                        <DashboardLayout />
+                    </PrivateRoute>
+                } />
+
             </Routes>
             <Footer />
         </>
