702a3bc0095e42361fc7caeebaa3e51517be4619
rahul2004a <kumar.rahul.2017354@gmail.com>
2025-06-05 23:40:42 +0530

    fix error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
-import { getApps } from './utils/Helper'
+import { getApps } from './utils/Helper.js'
 
 function App() {
   const CurrentApp = getApps();
